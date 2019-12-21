c41a0190c6fe5bce72fd7cf065684ed9f77c6275
Chu Huy <huychu9977@gmail.com>
2019-12-21 14:34:35 +0700

    huy

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-// export const SERVER_API_URL = 'http://52.74.230.96:8080/';
-export const SERVER_API_URL = 'http://localhost:8080/';
+export const SERVER_API_URL = 'http://52.74.230.96:8080/';
+// export const SERVER_API_URL = 'http://localhost:8080/';
